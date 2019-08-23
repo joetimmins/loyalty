59bb2c098ead77e95e1a901ee7a1f103f980d468
Frankie-Boy <frankieboylan@hotmail.co.uk>
2019-08-23 15:23:22 +0100

    (WIP) Introduce signup activity

--- app/src/main/java/com/novoda/loyalty/SignUpActivity.kt
+++ app/src/main/java/com/novoda/loyalty/SignUpActivity.kt
@@ -3,20 +3,17 @@ package com.novoda.loyalty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
 
         auth = FirebaseAuth.getInstance()
-    }
-
-    override fun onStart() {
-        super.onStart()
-
+        signup_button.setOnClickListener {  }
     }
 }
